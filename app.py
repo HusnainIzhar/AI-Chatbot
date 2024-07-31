2887199661b0c49c980d7b9520b513bfc50a485e
Husnain Izhar <husnainizhar@outlook.com>
2024-07-31 21:01:51 +0000

    latest

--- app.py
+++ app.py
@@ -1,18 +1,15 @@
 from llm import handle_chat
 import gradio as gr
 
-# print(handle_chat(file='Medical_book.pdf', question='what is this book about'))
-# print(normal_chat("what is today date "))
-
+# Gradio interface
 ui = gr.Interface(
-    fn=handle_chat,              
-    inputs=[gr.File(), 'text'],  
-    outputs=gr.Textbox(lines=14),              
-    title="Chatbot",        
+    fn=handle_chat,
+    inputs=[gr.File(), 'text'],
+    outputs=gr.Textbox(lines=14),
+    title="Chatbot",
     description="Upload a PDF file and ask a question to get an answer, or ask a question directly.",
-    theme=gr.themes.Default( primary_hue="violet",
-    secondary_hue="violet",)
+    theme=gr.themes.Default(primary_hue="violet", secondary_hue="violet")
 )
 
+print("its working", flush=True)
 ui.launch(server_name="0.0.0.0", server_port=3000)
-
